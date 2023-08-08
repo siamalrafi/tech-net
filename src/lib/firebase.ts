b900ef5c909e69980cb466d582aeeb3253400bf7
siamalrafi <rahathasandipto@gmail.com>
2023-08-08 17:23:15 +0600

    asynce

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,17 +1,17 @@
-import { getAuth } from 'firebase/auth';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.VITE_apiKey,
-  authDomain: process.env.VITE_authDomain,
-  projectId: process.env.VITE_projectId,
-  storageBucket: process.env.VITE_storageBucket,
-  messagingSenderId: process.env.VITE_messagingSenderId,
-  appId: process.env.VITE_appId,
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
--- src/redux/features/users/usersSlice.ts
+++ src/redux/features/users/usersSlice.ts
@@ -1,4 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { auth } from '@/lib/firebase';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 interface IUserState {
   user: {
@@ -17,11 +19,34 @@ const initialState: IUserState = {
   isError: false,
   error: null,
 };
+export const createUser = createAsyncThunk(
+  'user/create',
+  async ({ email, password }: { email: string; password: string }) => {
+    const data = await createUserWithEmailAndPassword(auth, email, password);
+    return data.user.email;
+  }
+);
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createUser.pending, (state) => {
+        state.isLoading = true;
+        (state.isError = false), (state.error = null);
+      })
+      .addCase(createUser.fulfilled, (state, action) => {
+        state.user.email = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.user.email = null;
+        state.isError = true;
+        state.error = action.error.message!;
+      });
+  },
 });
 
 // export const {} = userSlice.actions;
