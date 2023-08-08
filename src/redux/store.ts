945f4b37e749fbed18b10217afbea0f405adce23
siamalrafi <rahathasandipto@gmail.com>
2023-08-08 09:58:11 +0600

    new code

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cardReducer from './features/card/cardSlice';
 import productReducer from './features/products/productsSlice';
+import { api } from './api/apiSlice';
 
 export const store = configureStore({
   reducer: {
     card: cardReducer,
     product: productReducer,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
